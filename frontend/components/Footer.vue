7f3bcfaf2858547ceb1b5c62b5fcd68a785a1f50
Raphile14 <rllddalangin@gmail.com>
2022-03-08 16:43:56 +0800

    Footer improvements

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,33 +1,47 @@
 <template>
     <div id="footer">
-        <div class="footer__container">
-            <div class="footer__col">
-                <img
-                    class="footer__logo"
-                    src="/images/haru-logo@2x.png"
-                    alt="haru-logo"
-                />
-                <h2><strong>Follow Us</strong></h2>
+        <div class="footer__col">
+            <img
+                class="footer__logo"
+                src="/images/haru-logo@2x.png"
+                alt="haru-logo"
+            />
+            <div class="footer__1">
+                <h2>Follow Us</h2>
                 <div class="footer__socials footer__subtitle">
                     <div>
-                        <img src="/images/socials/facebook.svg" alt="visa" />
-                    </div>
-                    <div>
-                        <img src="/images/socials/instagram.svg" alt="visa" />
-                    </div>
-                    <div>
-                        <img src="/images/socials/pinterest.svg" alt="visa" />
+                        <a
+                            class="footer__socials__image"
+                            href="https://facebook.com"
+                            target="_blank"
+                            ><img src="/images/socials/facebook.svg" alt="visa"
+                        /></a>
+                        <a
+                            class="footer__socials__image"
+                            href="https://instagram"
+                            ><img
+                                src="/images/socials/instagram.svg"
+                                alt="visa"
+                        /></a>
+                        <a
+                            class="footer__socials__image"
+                            href="https://pinterest.com"
+                            target="_blank"
+                            ><img
+                                src="/images/socials/pinterest.svg"
+                                alt="visa"
+                        /></a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="">
+        <div class="footer__col">
             <h2><strong>About</strong></h2>
             <div>
                 <NuxtLink to="/our-story"> Our Story </NuxtLink>
             </div>
         </div>
-        <div class="">
+        <div class="footer__col">
             <h2><strong>Extras</strong></h2>
             <div>
                 <NuxtLink to="/terms-and-conditions">
@@ -37,10 +51,10 @@
                 <NuxtLink to="/privacy-policy"> Privacy Policy </NuxtLink>
             </div>
         </div>
-        <div class="">
+        <div class="footer__col">
             <h2><strong>We Accept</strong></h2>
             <div>Cash on Delivery</div>
-            <div>
+            <div class="footer__payments__container">
                 <div class="payments">
                     <img
                         src="/images/payments/mastercard.png"
@@ -61,7 +75,7 @@
                 </div>
             </div>
         </div>
-        <div class="footer__col">
+        <div class="footer__col__2">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="14.82"
@@ -94,22 +108,56 @@ export default {
 
 <style>
 #footer {
-    overflow: hidden;
-    white-space: nowrap;
-    padding-left: 6em;
+    /* overflow: hidden;
+    white-space: nowrap; */
+    /* padding-left: 6em;
     padding-right: 6em;
-    margin-bottom: 5em;
+    margin-bottom: 5em; */
     background-color: var(--white);
-}
-
-.footer__container {
     display: flex;
     padding: 40px 120px;
     position: relative;
 }
 
+.footer__socials {
+    display: flex;
+    max-width: 200px;
+}
+
+.footer__socials__image {
+    margin: 10px 5px;
+}
+
+.footer__payments__container {
+    display: flex;
+    flex-flow: row wrap;
+    max-width: 300px;
+    margin: 5px -10px;
+}
+
 .footer__col {
     flex: 0 0 25%;
+}
+
+.footer__col__2 {
+    position: absolute;
+    right: calc(10% - 30px);
+}
+
+.payments {
+    background-color: var(--white);
+    box-shadow: 0 3px 6px var(--box_shadow_gray);
+    margin: 5px 10px;
+    flex: 0 0 calc(33.33% - 20px);
+    padding: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+}
+
+.payments > img {
+    max-width: 45px;
 }
 
 .footer__logo {
@@ -127,7 +175,6 @@ export default {
 
 #footer > div,
 .footer__socials,
-.payments,
 .footer__socials > div {
     display: inline-block;
     vertical-align: text-top;
@@ -139,6 +186,16 @@ export default {
 
 #footer {
     color: var(--gray_2);
+}
+
+.footer__1 {
+    margin-top: 25px;
+}
+.footer__1 > h2 {
+    font-family: Karla-Bold;
+    font-size: 18px;
+    color: var(--secondary);
+    margin-bottom: 10px;
 }
 
 @media (max-width: 1155px) {
