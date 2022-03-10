dc0376280705669e9f9cc6e574cfe3fdd4ce7cdb
Raphile14 <rllddalangin@gmail.com>
2022-03-10 11:56:46 +0800

    Initial Login

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,5 +1,225 @@
 <template>
-    <div class="main"><p>login</p></div>
+    <div class="main">
+        <div class="login__container">
+            <img
+                class="login__img"
+                src="/images/home/haru.png"
+                style="height: 937px"
+            />
+            <div class="login__register">
+                <div class="login__process">
+                    <h2 class="login__header">Already a member?</h2>
+                    <span class="login__description"
+                        >Log into your account for faster checkout.</span
+                    >
+                    <div data-v-007948fa="" class="login__wrapper">
+                        <form data-v-007948fa="" data-vv-scope="login__form">
+                            <div data-v-007948fa="" class="form_group">
+                                <label data-v-007948fa="" for="email"
+                                    >Email</label
+                                >
+                                <input
+                                    data-v-007948fa=""
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    autocomplete="off"
+                                    placeholder="Enter your email address"
+                                    class="input_text"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                />
+                                <div
+                                    data-v-007948fa=""
+                                    class="validation_container"
+                                >
+                                    <span
+                                        data-v-007948fa=""
+                                        class="validation_errors login_errors"
+                                        style="display: none"
+                                    ></span>
+                                </div>
+                            </div>
+                            <div data-v-007948fa="" class="form_group">
+                                <label data-v-007948fa="" for="password"
+                                    >Password</label
+                                >
+                                <input
+                                    data-v-007948fa=""
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    autocomplete="off"
+                                    placeholder="Enter your password"
+                                    class="input_text"
+                                    aria-required="true"
+                                    aria-invalid="false"
+                                />
+                                <div
+                                    data-v-007948fa=""
+                                    class="validation_container"
+                                >
+                                    <span
+                                        data-v-007948fa=""
+                                        class="validation_errors login_errors"
+                                        style="display: none"
+                                    ></span>
+                                </div>
+                            </div>
+                            <div data-v-007948fa="" class="remember_forgot">
+                                <div data-v-007948fa="" class="left">
+                                    <input
+                                        data-v-007948fa=""
+                                        type="checkbox"
+                                        id="remember_me"
+                                        name="rememberme"
+                                        class="input_check"
+                                    />
+                                    <label data-v-007948fa="" for="remember_me"
+                                        >Remember me</label
+                                    >
+                                </div>
+                                <div data-v-007948fa="" class="right">
+                                    <a
+                                        data-v-007948fa=""
+                                        href="/forgot-password"
+                                        class=""
+                                        >Forgot Password?</a
+                                    >
+                                </div>
+                            </div>
+                            <button
+                                data-v-007948fa=""
+                                type="submit"
+                                class="submit primary__button"
+                            >
+                                Login
+                            </button>
+                        </form>
+                        <div data-v-007948fa="" class="error"><!----></div>
+                        <div data-v-007948fa="" class="others">
+                            <div data-v-007948fa="" class="platforms__socials">
+                                <a
+                                    data-v-007948fa=""
+                                    href="javascript:;"
+                                    class="left"
+                                    ><svg
+                                        data-v-007948fa=""
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="55.281"
+                                        height="55.281"
+                                        viewBox="0 0 55.281 55.281"
+                                    >
+                                        <defs data-v-007948fa="">
+                                            <style data-v-007948fa="">
+                                                .a {
+                                                    fill: #3b5998;
+                                                }
+                                                .b {
+                                                    fill: #fff;
+                                                }
+                                            </style>
+                                        </defs>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M27.641,0A27.641,27.641,0,1,1,0,27.641,27.641,27.641,0,0,1,27.641,0Z"
+                                            class="a"
+                                            style="
+                                                fill: rgb(59, 89, 152);
+                                                color: white;
+                                            "
+                                        ></path>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M119.432,77.809h3.562V72.547h-4.187v.019c-5.074.18-6.114,3.032-6.205,6.027h-.01v2.628h-3.455v5.153h3.455v13.813H117.8V86.374h4.265l.824-5.153H117.8V79.633A1.7,1.7,0,0,1,119.432,77.809Z"
+                                            transform="translate(-88.426 -58.78)"
+                                            class="b"
+                                            style="fill: rgb(255, 255, 255)"
+                                        ></path></svg
+                                ></a>
+                                <a
+                                    data-v-007948fa=""
+                                    href="javascript:;"
+                                    class="right"
+                                    ><svg
+                                        data-v-007948fa=""
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="28.237"
+                                        height="28.237"
+                                        viewBox="0 0 28.237 28.237"
+                                    >
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M57.594,303.87A13.961,13.961,0,0,1,36.4,300.28l.917-4.226,4.063-.754a7.5,7.5,0,0,0,7.131,5.278,7.339,7.339,0,0,0,4.368-1.423l3.9.6Z"
+                                            transform="translate(-34.393 -279.014)"
+                                            class="a"
+                                        ></path>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M265.083,369.915l-.811-4.12-3.9-.6A7.339,7.339,0,0,1,256,366.623V373.3A14.1,14.1,0,0,0,265.083,369.915Z"
+                                            transform="translate(-241.882 -345.059)"
+                                            class="b"
+                                        ></path>
+                                        <g
+                                            data-v-007948fa=""
+                                            transform="translate(0 6.971)"
+                                        >
+                                            <path
+                                                data-v-007948fa=""
+                                                d="M6.673,133.547a7.71,7.71,0,0,0,.314,2.167l-4.98,4.98a13.728,13.728,0,0,1,0-14.295l4,.688.983,4.292A7.709,7.709,0,0,0,6.673,133.547Z"
+                                                transform="translate(0 -126.4)"
+                                                class="c"
+                                            ></path>
+                                        </g>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M255.946,199.309a14.1,14.1,0,0,1-5.035,10.738l-4.715-4.715a7.321,7.321,0,0,0,2.3-2.713h-6.668a.819.819,0,0,1-.827-.827v-4.964a.819.819,0,0,1,.827-.827h13.06a.818.818,0,0,1,.811.678A14.032,14.032,0,0,1,255.946,199.309Z"
+                                            transform="translate(-227.709 -185.191)"
+                                            class="d"
+                                        ></path>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M262.668,202.618a7.321,7.321,0,0,1-2.3,2.713l4.715,4.715a13.983,13.983,0,0,0,4.787-13.368.817.817,0,0,0-.811-.678H256v6.618Z"
+                                            transform="translate(-241.882 -185.191)"
+                                            class="e"
+                                        ></path>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M57.826,3.943a.835.835,0,0,1-.232.612L54.054,8.079a.81.81,0,0,1-1.075.083,7.4,7.4,0,0,0-4.467-1.489,7.5,7.5,0,0,0-7.131,5.278L36.4,6.971a13.953,13.953,0,0,1,21.128-3.64.87.87,0,0,1,.3.612Z"
+                                            transform="translate(-34.393 0)"
+                                            class="f"
+                                        ></path>
+                                        <path
+                                            data-v-007948fa=""
+                                            d="M260.467,8.162a.81.81,0,0,0,1.075-.083l3.541-3.524a.834.834,0,0,0,.232-.612.87.87,0,0,0-.3-.612A14.076,14.076,0,0,0,256,0V6.673A7.4,7.4,0,0,1,260.467,8.162Z"
+                                            transform="translate(-241.882 0)"
+                                            class="g"
+                                        ></path></svg
+                                ></a>
+                            </div>
+                        </div>
+                        <div data-v-007948fa="" class="register">
+                            <p data-v-007948fa="">
+                                New here?
+                                <a
+                                    data-v-007948fa=""
+                                    href="/register"
+                                    class="register_btn"
+                                    >Create an account</a
+                                >
+                            </p>
+                        </div>
+                        <a
+                            data-v-007948fa=""
+                            href="/"
+                            class="btn_guest nuxt-link-active"
+                            >Order as Guest</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,4 +234,126 @@ export default {
 </script>
 
 <style>
+.register {
+    margin: 27px 0 16px;
+    text-align: center;
+}
+.register_btn {
+    /* font-size: 14px; */
+    transition: all 0.3s ease-out;
+    text-transform: none;
+    font-family: Poppins-Regular;
+    color: var(--text-primary);
+}
+.btn_guest {
+    border: 1px solid var(--primary);
+    padding: 6px 15px 5px;
+    line-height: 26px;
+    color: var(--primary);
+    border-radius: 8px;
+    font-size: 16px;
+    text-transform: none;
+    font-family: Poppins-Regular;
+    cursor: pointer;
+    text-align: center;
+    transition: 0.2s ease-in-out;
+}
+.remember_forgot > .left {
+    display: flex;
+    align-items: center;
+    height: 20px;
+}
+.remember_forgot > .right {
+    user-select: none;
+    box-sizing: border-box;
+    margin: 0;
+}
+.remember_forgot > div > a {
+    font-size: 14px;
+    text-transform: none;
+    font-family: Poppins-Regular;
+    color: var(--tertiary);
+    transition: all 0.3s ease-out;
+}
+.remember_forgot > .left > .input_check {
+    border: 1px solid hsla(0, 0%, 43.9%, 0.5);
+    width: 24px !important;
+    height: 24px !important;
+    border: 1px solid hsla(0, 0%, 43.9%, 0.5);
+    cursor: pointer;
+    position: relative;
+    border-radius: 5px;
+    transition: 0.2s ease-in-out;
+}
+.remember_forgot > .left > label {
+    font-size: 14px;
+    text-transform: none;
+    font-family: Poppins-Regular;
+    color: #383838;
+    margin: 0 7px;
+}
+.login__form {
+    user-select: none;
+    box-sizing: border-box;
+    margin: 0;
+}
+.login__wrapper {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    margin-top: 33px;
+    width: 100%;
+    max-width: 294px;
+}
+.login__description {
+    font-family: Karla-Regular;
+    line-height: 23px;
+}
+.login__header {
+    font-family: Karla-Bold;
+    font-size: 33px;
+    line-height: 43px;
+    margin-bottom: 0;
+}
+.login__process {
+    position: absolute;
+    top: 40px;
+    right: 50px;
+    background-color: #fff;
+    border-radius: 15px;
+    padding: 68px 50px;
+    width: 100%;
+    max-width: 573px;
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+    color: #383838;
+}
+.login__register {
+    user-select: none;
+    box-sizing: border-box;
+    margin: 0;
+}
+.login__img {
+    width: 100%;
+    object-fit: cover;
+    object-position: top;
+}
+.login__container {
+    width: 100%;
+    /* max-width: 80%; */
+    margin: 0 auto;
+    margin-top: 6em;
+}
+.platforms__socials > .left {
+    background-color: #3b5998;
+    box-shadow: 0 3px 6px rgb(0 0 0 / 16%);
+    border-radius: 5px;
+}
+.platforms__socials > .right {
+    background-color: #fff;
+    box-shadow: 0 3px 6px rgb(0 0 0 / 16%);
+    border-radius: 5px;
+    flex-grow: 1;
+}
 </style>
